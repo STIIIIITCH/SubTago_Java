6e92b0e88ac1ac544963e7128fdf86609b47111c
STIIIIICH <djaak1435@gmail.com>
2022-05-31 14:45:00 +0900

    java

--- app/src/main/java/com/example/subtago_java/TransferActivity.java
+++ app/src/main/java/com/example/subtago_java/TransferActivity.java
@@ -9,5 +9,5 @@ public class TransferActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transfer_1);
-}
+    }
 }
