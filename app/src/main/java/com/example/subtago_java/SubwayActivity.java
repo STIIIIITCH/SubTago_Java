6010c213692d7e14fc15a6d973074d8c6d56b519
ggulbbang24 <stellaor24@gmail.com>
2022-06-19 20:16:10 +0900

    <이미지 좌표 수정> 이미지 크기가 왜 원본 이미지의 3배 정도로 되는지 모르겠음 암튼 좌표는 이동/축소/확대 상관없이 같게 나옴

--- app/src/main/java/com/example/subtago_java/SubwayActivity.java
+++ app/src/main/java/com/example/subtago_java/SubwayActivity.java
@@ -1,6 +1,7 @@
 package com.example.subtago_java;
 
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.graphics.PointF;
@@ -25,6 +26,7 @@ public class SubwayActivity extends AppCompatActivity {
     private static final String TAG = "* 클릭 좌표";
     Cursor c = null;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,46 +59,7 @@ public class SubwayActivity extends AppCompatActivity {
             return false;
         });
 
-
-
-//        float[] values = new float[9];
-//        matrix.getValues(values);
-//        // values[2] and values[5] are the x,y coordinates of the top left corner of the drawable image, regardless of the zoom factor.
-//        // values[0] and values[4] are the zoom factors for the image's width and height respectively. If you zoom at the same factor, these should both be the same value.
-//
-//        // event is the touch event for MotionEvent.ACTION_UP
-//        float relativeX = (event.getX() - values[2]) / values[0];
-//        float relativeY = (event.getY() - values[5]) / values[4];
-
-
-//        gestureDetector = new GestureDetectorCompat(this, new MyGestureListener());
-
-
-
     }
-
-
-/*    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        @Override
-        public boolean onDown(MotionEvent touch) {
-            if (imageView.isReady()) {
-                PointF sCoord = imageView.viewToSourceCoord(touch.getX(), touch.getY());
-                int x_cor = (int) sCoord.x;
-                int y_cor = (int) sCoord.y;
-
-                // Loop for finding the station.
-                if (c.moveToFirst()) {
-                    do {
-                        Log.d("커서 테스트 >> ",  "커서 꺼짐 ? " + c.isClosed() + "커서 마지막인가요 ? " + c.isLast() + "커서 첫 번째인지" + String.valueOf(c.isFirst()));
-                        if ((x_cor > c.getInt(2)) && (x_cor < c.getInt(4)) && (y_cor > c.getInt(3)) && (y_cor < c.getInt(5))) {
-                            String targetStation = c.getString(1); // 유저가 클릭한 지하철역
-                        } // send Station Name (column 1)
-                    } while (c.moveToNext());
-                }
-            }
-            return super.onSingleTapUp(touch);
-        }
-    };*/
 
     private GestureDetector getGestureDetector(SubsamplingScaleImageView subsamplingScaleImageView)
     {
@@ -134,42 +97,6 @@ public class SubwayActivity extends AppCompatActivity {
                                 if ((x_cor > c.getInt(2)) && (x_cor < c.getInt(4)) && (y_cor > c.getInt(3)) && (y_cor < c.getInt(5))) {
                                     String targetStation = c.getString(1); // 좌표를 통해 얻은 유저가 클릭한 지하철역
                                     Log.d("# 픽셀좌표 테스트 2 >> ", "targetStation : " + targetStation);
-//                                    CustomDialog customDialog = new CustomDialog(mContext, event.getX(), event.getY(),  targetStation);
-//                                    customDialog.create();
-
-/*                                    customDialog.getDialog().setOnDismissListener(new DialogInterface.OnDismissListener() {
-                                        @Override
-                                        public void onDismiss(DialogInterface dialog) {
-
-
-                                            TextView tvSearchStation = findViewById(R.id.tv_serach_station);
-                                            StringBuilder sb = new StringBuilder();
-
-                                            if(StationData.startStation!=null && StationData.startLine!= null){
-
-                                                sb.append("출발역 : ");
-                                                sb.append(StationData.startStation);
-                                                sb.append(" ");
-                                                sb.append(StationData.startLine);
-                                                sb.append("호선");
-                                                sb.append("\n");
-                                            }
-
-                                            if(StationData.goalStation!=null && StationData.goalLine!= null){
-
-                                                sb.append("도착역 : ");
-                                                sb.append(StationData.goalStation);
-                                                sb.append(" ");
-                                                sb.append(StationData.goalLine);
-                                                sb.append("호선");
-                                            }
-
-
-                                            tvSearchStation.setText(sb.toString());
-
-
-                                        }
-                                    });*/
 
                                 } // send Station Name (column 1)
                             } while (c.moveToNext());
@@ -181,11 +108,6 @@ public class SubwayActivity extends AppCompatActivity {
             });
 
         }
-
         return gestureDetector;
-
-
-
     }
-
 }
